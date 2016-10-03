fa661a74cf0e50158f0b1a5b2fd4b622902bd2fe
Benjamin Pasero <benjpas@microsoft.com>
2016-10-03 15:13:37 +0200

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,14 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    let disposable = vscode.commands.registerCommand('extension.closeAll', () => {
+        vscode.commands.executeCommand('workbench.action.closeEditorsInGroup');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
